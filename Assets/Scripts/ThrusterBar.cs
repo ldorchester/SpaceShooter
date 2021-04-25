ab613b497149c747fdf7c4e63f7202baa37f1e89
ldorchester <ldorchester@mtroyal.ca>
2021-04-25 12:26:19 -0600

    Add Thruster Bar Gauge

---
+++ Assets/Scripts/ThrusterBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThrusterBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxThruster(float thruster)
+    {
+        slider.maxValue = thruster;
+        slider.value = thruster;
+    }
+
+    public void SetThruster(float thruster)
+    {
+        slider.value = thruster;
+    }
+}
